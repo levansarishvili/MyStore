a7ecd6a0247863d37aaef96013518ac8158e9607
Levani Sarishvili <levanisarishvili322@gmail.com>
2024-09-22 21:30:19 +0400

    finished building header component with styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
 import Header from "./Header.js";
 import Main from "./Main.js";
 import Footer from "./Footer.js";
+import "./App.css";
 
 // Create App component
 function App() {
--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,31 @@ import "./Header.css";
 
 // Create Header component
 function Header() {
-  return <p>It is Header Section</p>;
+  return (
+    <header className="header">
+      <div className="header__wrapper">
+        <img
+          src="./assets/logo.svg"
+          alt="Georgia national football team logo"
+          className="header__logo"
+        ></img>
+        <nav className="header__nav">
+          <ul className="nav__list">
+            <li className="list__item">მთავარი</li>
+            <li className="list__item">შემადგენლობა</li>
+            <li className="list__item">სიახლეები</li>
+            <li className="list__item">მაღაზია</li>
+          </ul>
+          <Button />
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+// Create Button component
+function Button() {
+  return <button className="btn">ბილეთები</button>;
 }
 
 // Exporting Header component
