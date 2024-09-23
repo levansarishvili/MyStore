681cfc25e7a5d4a20da779e3bd45a36be36a0c0f
Levani Sarishvili <levanisarishvili322@gmail.com>
2024-09-23 13:08:01 +0400

    finished building main component and also add styles

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,81 @@
 import "./Main.css";
+import playerData from "./Data.js";
 
 // Create Main component
 function Main() {
-  return <p>It is Main section</p>;
+  const formation = {
+    goalkeeper: [playerData[0]],
+    defence: [playerData[1], playerData[2], playerData[3], playerData[4]],
+    midfield: [playerData[5], playerData[6], playerData[7]],
+    attack: [playerData[8], playerData[9], playerData[10]],
+  };
+
+  return (
+    <main className="main">
+      {/* Attack */}
+      <div className="attack line">
+        {formation.attack.map((player, index) => (
+          <Player
+            key={index}
+            playerName={player.name}
+            playerImageSrc={player.image}
+            playerClub={player.club}
+          />
+        ))}
+      </div>
+
+      {/* Midfield */}
+      <div className="midfield line">
+        {formation.midfield.map((player, index) => (
+          <Player
+            key={index}
+            playerName={player.name}
+            playerImageSrc={player.image}
+            playerClub={player.club}
+          />
+        ))}
+      </div>
+
+      {/* Defence */}
+      <div className="defence line">
+        {formation.defence.map((player, index) => (
+          <Player
+            key={index}
+            playerName={player.name}
+            playerImageSrc={player.image}
+            playerClub={player.club}
+          />
+        ))}
+      </div>
+
+      {/* Goalkeeper */}
+      <div className="goalkeeper line">
+        {formation.goalkeeper.map((player, index) => (
+          <Player
+            key={index}
+            playerName={player.name}
+            playerImageSrc={player.image}
+            playerClub={player.club}
+          />
+        ))}
+      </div>
+    </main>
+  );
+}
+
+// Player card component
+function Player({ playerName, playerImageSrc, playerClub }) {
+  return (
+    <div className="player-card">
+      <img
+        src={playerImageSrc}
+        className="player__img"
+        alt="Georgian football player"
+      ></img>
+      <p className="player__name">{playerName}</p>
+      <p className="player__club">{playerClub}</p>
+    </div>
+  );
 }
 
 // Exporting Main component
