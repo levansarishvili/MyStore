91384990b8162847bfea4b9a9e9bc793dc25ddfe
Levani Sarishvili <levanisarishvili322@gmail.com>
2024-09-25 17:44:45 +0400

    feat:(online store) add online store feature with its styles

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 import "./Main.css";
-import playerData from "./Data.js";
+import playerData from "./playerData.js";
+import productData from "./productData.js";
 
 // Create Main component
 function Main() {
@@ -12,53 +13,72 @@ function Main() {
 
   return (
     <main className="main">
-      {/* Attack */}
-      <div className="attack line">
-        {formation.attack.map((player, index) => (
-          <Player
-            key={index}
-            playerName={player.name}
-            playerImageSrc={player.image}
-            playerClub={player.club}
-          />
-        ))}
-      </div>
+      {/* Fomration */}
+      <section className="formation-wrapper">
+        <h1 className="section__header">
+          საქართველოს ფეხბურთის ეროვნული ნაკრები
+        </h1>{" "}
+        {/* Attack */}
+        <div className="attack line">
+          {formation.attack.map((player, index) => (
+            <Player
+              key={index}
+              playerName={player.name}
+              playerImageSrc={player.image}
+              playerClub={player.club}
+            />
+          ))}
+        </div>
+        {/* Midfield */}
+        <div className="midfield line">
+          {formation.midfield.map((player, index) => (
+            <Player
+              key={index}
+              playerName={player.name}
+              playerImageSrc={player.image}
+              playerClub={player.club}
+            />
+          ))}
+        </div>
+        {/* Defence */}
+        <div className="defence line">
+          {formation.defence.map((player, index) => (
+            <Player
+              key={index}
+              playerName={player.name}
+              playerImageSrc={player.image}
+              playerClub={player.club}
+            />
+          ))}
+        </div>
+        {/* Goalkeeper */}
+        <div className="goalkeeper line">
+          {formation.goalkeeper.map((player, index) => (
+            <Player
+              key={index}
+              playerName={player.name}
+              playerImageSrc={player.image}
+              playerClub={player.club}
+            />
+          ))}
+        </div>
+      </section>
 
-      {/* Midfield */}
-      <div className="midfield line">
-        {formation.midfield.map((player, index) => (
-          <Player
-            key={index}
-            playerName={player.name}
-            playerImageSrc={player.image}
-            playerClub={player.club}
-          />
-        ))}
-      </div>
-
-      {/* Defence */}
-      <div className="defence line">
-        {formation.defence.map((player, index) => (
-          <Player
-            key={index}
-            playerName={player.name}
-            playerImageSrc={player.image}
-            playerClub={player.club}
-          />
-        ))}
-      </div>
-
-      {/* Goalkeeper */}
-      <div className="goalkeeper line">
-        {formation.goalkeeper.map((player, index) => (
-          <Player
-            key={index}
-            playerName={player.name}
-            playerImageSrc={player.image}
-            playerClub={player.club}
-          />
-        ))}
-      </div>
+      <section className="store-wrapper">
+        <h1 className="section__header">GFF OFFICIAL FANSHOP</h1>
+        <div className="store">
+          {productData.map((product, index) => (
+            <Product
+              key={index}
+              name={product.name}
+              imageSrc={product.image}
+              color={product.color}
+              size={product.size}
+              price={product.price}
+            />
+          ))}
+        </div>
+      </section>
     </main>
   );
 }
@@ -76,6 +96,26 @@ function Player({ playerName, playerImageSrc, playerClub }) {
       <p className="player__club">{playerClub}</p>
     </div>
   );
+}
+
+// Product card component
+function Product({ name, imageSrc, color, size, price }) {
+  return (
+    <div className="product-card">
+      <img className="product__img" src={imageSrc} alt="Product"></img>
+      <h2 className="product__title">{name}</h2>
+      <div className="product__desc">
+        <p className="product__color">{color}</p>
+        <p className="product__price">{`${price}.00 ₾`}</p>
+      </div>
+      <Button />
+    </div>
+  );
+}
+
+// Create Button component
+function Button() {
+  return <button className="btn">Add to Cart</button>;
 }
 
 // Exporting Main component
---
+++ src/productData.js
@@ -0,0 +1,87 @@
+const productData = [
+  {
+    name: "ეროვნული ნაკრების 2023 წლის სათამაშო მაისური",
+    image: "./assets/black-shirt.png",
+    color: "შავი",
+    size: "M",
+    price: 220,
+  },
+  {
+    name: "ეროვნული ნაკრების 2023 წლის სათამაშო მაისური",
+    image: "./assets/red-shirt.png",
+    color: "წითელი",
+    size: "L",
+    price: 220,
+  },
+  {
+    name: "ეროვნული ნაკრების 2023 წლის სათამაშო მაისური",
+    image: "./assets/white-shirt.png",
+    color: "თეთრი",
+    size: "L",
+    price: 220,
+  },
+  {
+    name: "ეროვნული ნაკრების 2023 სეზონის შორტი",
+    image: "./assets/white-short.jpg",
+    color: "თეთრი",
+    size: "M",
+    price: 80,
+  },
+  {
+    name: "ეროვნული ნაკრების 2023 სეზონის შორტი",
+    image: "./assets/black-short.jpg",
+    color: "შავი",
+    size: "M",
+    price: 80,
+  },
+  {
+    name: "ჰუდი 2022",
+    image: "./assets/black-hoodie.jpg",
+    color: "შავი",
+    size: "XL",
+    price: 120,
+  },
+  {
+    name: "ჰუდი 2022",
+    image: "./assets/white-hoodie.jpg",
+    color: "თეთრი",
+    size: "L",
+    price: 120,
+  },
+  {
+    name: "პულოვერი 2022",
+    image: "./assets/white-pullover.jpg",
+    color: "თეთრი",
+    size: "L",
+    price: 100,
+  },
+  {
+    name: "პულოვერი 2022",
+    image: "./assets/black-pullover.jpg",
+    color: "შავი",
+    size: "L",
+    price: 100,
+  },
+
+  {
+    name: "ჩანთა ანიმაციით",
+    image: "./assets/black-bag.jpg",
+    color: "შავი",
+    price: 45,
+  },
+  {
+    name: "ჩანთა ანიმაციით",
+    image: "./assets/white-bag.jpg",
+    color: "თეთრი",
+    price: 45,
+  },
+  {
+    name: "ჩანთა ანიმაციით",
+    image: "./assets/red-bag.jpg",
+    color: "წითელი",
+    price: 45,
+  },
+];
+
+// Exporting playerData
+export default productData;
