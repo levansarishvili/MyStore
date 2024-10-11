197945bde28f9e913d02408a3d8b7760698fc766
Levani Sarishvili <levanisarishvili322@gmail.com>
2024-10-11 18:23:05 +0400

    fix: add live link in readme file and also update next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: "expor t", // Outputs a Single-Page Application (SPA).
-  // distDir: "./build", // Changes the build output directory to `./dist`.
+  distDir: "./build", // Changes the build output directory to `./dist`.
 };
 
 export default nextConfig;
