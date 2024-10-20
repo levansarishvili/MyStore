2271650488dc6a075b51e172ea7c47c41f8c842f
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-10-20 22:52:20 +0400

    refactor: main page to handle message for authorized/nonauthorized user

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,59 @@
-import "./Homepage.css";
-import Link from "next/link";
+// app/page.jsx
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
-function Homepage() {
+export default function HomePage() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (!accessToken) {
+      router.push("/login");
+    } else {
+      const fetchUserProfile = async () => {
+        try {
+          const response = await fetch("https://dummyjson.com/auth/me", {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+            // credentials: "include",
+          });
+
+          const data = await response.json();
+          if (response.ok) {
+            setUser(data);
+          } else {
+            console.error("Error fetching user data");
+            localStorage.removeItem("accessToken");
+            router.push("/login");
+          }
+        } catch (error) {
+          console.error("An error occurred:", error);
+          router.push("/login");
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchUserProfile();
+    }
+  }, [router]);
+
+  if (loading) return <p>Loading...</p>;
+
   return (
-    <section className="home-wrapper">
-      <h1 className="section-header">This is a homepage </h1>
-    </section>
+    <div>
+      <h1>Welcome to My Next.js App</h1>
+      {user && (
+        <p>
+          Hello, {user.firstName}! Explore the app and manage your products and
+          blog posts.
+        </p>
+      )}
+    </div>
   );
 }
-
-export default Homepage;
