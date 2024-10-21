39903f3f143ea1ef0158cc4543577cb775c7e2a1
levani_22 <97299346+levansarishvili@users.noreply.github.com>
2024-10-21 16:09:25 +0400

    feat: add api route for login

---
+++ src/app/api/login/route.js
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  const { username, password } = await req.json();
+  try {
+    const response = await fetch("https://dummyjson.com/user/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username,
+        password,
+        expiresInMins: 30,
+      }),
+    });
+    if (!response.ok) {
+      return NextResponse.json(
+        { error: "Password or Username is incorrect" },
+        { status: 401 }
+      );
+    }
+    const data = await response.json();
+    const accessToken = data.accessToken;
+
+    // Set token in cookies
+    const responseWithCookies = NextResponse.json({
+      message: "Login successful",
+    });
+    responseWithCookies.cookies.set("accessToken", accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 60 * 30,
+      path: "/",
+    });
+    return responseWithCookies;
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
