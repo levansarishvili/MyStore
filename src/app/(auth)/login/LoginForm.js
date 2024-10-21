929cb5ac98563b5953593f117dcea531bd9c53b2
levani_22 <97299346+levansarishvili@users.noreply.github.com>
2024-10-21 16:04:31 +0400

    feat: create authorization functionality using client side code

---
+++ src/app/(auth)/login/LoginForm.js
@@ -0,0 +1,27 @@
+// Async function to handle user login
+export default async function LoginUser(username, password) {
+  try {
+    const res = await fetch("https://dummyjson.com/user/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username,
+        password,
+        expiresInMins: 30,
+      }),
+    });
+    // Check if the response is successful
+    if (!res.ok) {
+      throw new Error("Password or Username is incorrect");
+    }
+    const data = await res.json();
+    console.log(data);
+    const accessToken = data.accessToken;
+
+    // Save accessToken into local storage
+    localStorage.setItem("accessToken", accessToken);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,25 +1,55 @@
-import "./LoginPage.css";
+"use client";
+import "./LoginForm.css";
 import Button from "../../components/Button";
-function LoginPage() {
+import LoginUser from "./LoginForm";
+import { useState } from "react";
+
+export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // Input change handlers
+  function handleUsernameChange(e) {
+    setUsername(e.target.value);
+  }
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+  }
+  // Handle form submission
+  async function handleSubmit(e) {
+    e.preventDefault();
+    LoginUser(username, password);
+
+    if (localStorage.getItem("accessToken")) {
+      window.location.href = "/store";
+    }
+  }
   return (
     <div className="login-page__wrapper">
       <h1 className="section-header">Login Form</h1>
       <div className="login-form-wrapper">
         {/* Login form */}
-        <form className="login-form">
+        <form className="login-form" onSubmit={handleSubmit}>
           <label className="login-input__label" htmlFor="username">
             <p className="login-label__txt">Username:</p>
-            <input className="login-input" type="text" id="username"></input>
+            <input
+              value={username}
+              className="login-input"
+              type="text"
+              id="username"
+              onChange={handleUsernameChange}
+            />
           </label>
           <label className="login-input__label" htmlFor="password">
             <p className="login-label__txt">Password:</p>
             <input
+              value={password}
               className="login-input"
               type="password"
               id="password"
-            ></input>
+              onChange={handlePasswordChange}
+            />
           </label>
-          <Button className="btn login-button" name="SIGN IN" />
+          <Button type="submit" className="btn login-button" name="Sign in" />
         </form>
         <div className="login-footer">
           <p className="login-footer-txt">
@@ -33,4 +63,3 @@ function LoginPage() {
     </div>
   );
 }
-export default LoginPage;
