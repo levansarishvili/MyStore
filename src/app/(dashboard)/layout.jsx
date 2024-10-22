2274acb333b0eec764b6a88a12d7fc5cc9c9f381
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-10-22 12:16:57 +0400

    fix: layout to redirect to the corresponding page

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -1,4 +1,14 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
 export default async function DashboardLayout({ children }) {
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+
+  if (!accessToken) {
+    return redirect("/login");
+  }
+
   return (
     <>
       {/* <Header /> */}
