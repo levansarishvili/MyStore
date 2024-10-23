581bfdbac1198b97e2126e6f6f750212372f1d1c
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-10-23 17:07:56 +0400

    feat(blog): handle form submission & save new post to local storage

--- src/app/(dashboard)/blog/page.js
+++ src/app/(dashboard)/blog/page.js
@@ -2,10 +2,12 @@
 // import Loading from "./loading";
 import "./Blog.css";
 import BlogItem from "./BlogItem";
+import BlogForm from "./BlogForm";
 import BlogFilter from "../../components/BlogFilter";
 
 import usePostsUrl from "../../hooks/usePostUrl";
 import useFetchPosts from "../../hooks/useFetchPosts";
+import { useState, useEffect } from "react";
 import { handleDelete } from "../../components/handleDelete";
 
 export default function BlogPage({ searchParams }) {
@@ -15,23 +17,41 @@ export default function BlogPage({ searchParams }) {
   const postsUrl = usePostsUrl(searchQuery, sortOptions);
   const { posts, setPosts } = useFetchPosts(postsUrl);
 
+  // fetch posts from local storage when the component mounts
+  useEffect(() => {
+    const savedPosts = JSON.parse(localStorage.getItem("posts")) || [];
+    if (savedPosts.length) {
+      setPosts((prevPosts) => [...prevPosts, ...savedPosts]);
+    }
+  }, [setPosts]);
+
+  const handleAddPost = (newPost) => {
+    setPosts((prevPosts) => [...prevPosts, newPost]);
+
+    const existingPosts = JSON.parse(localStorage.getItem("posts")) || [];
+    localStorage.setItem("posts", JSON.stringify([...existingPosts, newPost]));
+  };
+
   return (
     <section className="blog-wrapper">
       <h1 className="section-header">Blogs</h1>
       <div className="blog__page-content">
         <BlogFilter />
-        <ul className="blog__list">
-          {posts.map((post) => (
-            <BlogItem
-              key={post.id}
-              id={post.id}
-              title={post.title}
-              content={post.body}
-              views={post.views}
-              onDelete={() => handleDelete(posts, "posts", post.id, setPosts)}
-            />
-          ))}
-        </ul>
+        <div>
+          <BlogForm onAddPost={handleAddPost} />
+          <ul className="blog__list">
+            {posts.map((post) => (
+              <BlogItem
+                key={post.id}
+                id={post.id}
+                title={post.title}
+                content={post.body}
+                views={post.views}
+                onDelete={() => handleDelete(posts, "posts", post.id, setPosts)}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
   );
