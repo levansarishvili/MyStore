a516d6093587679b6e0c7f9efe6f7b2e0a4652e7
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-10-23 10:43:05 +0400

    fix: dashboard layout to check authorized user

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -1,12 +1,24 @@
-import { redirect } from "next/navigation";
+"use client";
 import Header from "../components/Header.js";
 import Footer from "../components/Footer.js";
 import "../../../src/global.css";
+import { redirect, useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default async function DashboardLayout({ children }) {
-  // if (!accessToken) {
-  //   return redirect("/login");
-  // }
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkAuthentication = () => {
+      const isAuthenticated = localStorage.getItem("auth");
+      if (!isAuthenticated) {
+        redirect("/login");
+      }
+    };
+    if (typeof window !== "undefined") {
+      checkAuthentication();
+    }
+  }, [router]);
 
   return (
     <>
