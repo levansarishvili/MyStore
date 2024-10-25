b9e0b5991b3fa63cbce28766355fbbd68b65e621
juglika <lashajugeli96@gmail.com>
2024-10-25 16:58:35 +0400

    refactor: implement auth custom hooks in login page

--- src/app/hooks/useAuth.js
+++ src/app/hooks/useAuth.js
@@ -26,11 +26,13 @@ export function useAuth() {
       }
 
       const data = await res.json();
+
+      // If login is successful, store authentication status and navigate to home
       localStorage.setItem("isAuth", JSON.stringify(true));
       localStorage.setItem("accessToken", data.accessToken);
       return true;
     } catch (err) {
-      setError(err.message);
+      setError("Invalid username or password. Please try again.");
       return false;
     } finally {
       setLoading(false);
@@ -39,36 +41,3 @@ export function useAuth() {
 
   return { checkAuth, loading, error };
 }
-
-//   // Function to check if the user credentials are correct
-//   async function checkAuth(username, password) {
-//     setLoading(true);
-//     setError(null);
-
-//     try {
-//       const res = await fetch("https://dummyjson.com/auth/login", {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify({
-//           username,
-//           password,
-//           expiresInMins: 60 * 24 * 7,
-//         }),
-//       });
-
-//       if (!res.ok) {
-//         throw new Error("Invalid username or password. Please try again.");
-//       }
-
-//       const data = await res.json();
-
-//       // If login is successful, store authentication status and navigate to home
-//       localStorage.setItem("isAuth", JSON.stringify(true));
-//       localStorage.setItem("accessToken", data.accessToken);
-//       router.push("/home");
-//     } catch (error) {
-//       setError("Invalid username or password. Please try again.");
-//     } finally {
-//       setLoading(false);
-//     }
-//   }
