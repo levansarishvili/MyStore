559748bd60bf7e4af05d65353637f1ce0cd9d4a5
juglika <lashajugeli96@gmail.com>
2024-10-25 19:20:19 +0400

    refactor: create custom hook for dashboard layout

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -3,26 +3,11 @@
 import Header from "../components/Header.js";
 import Footer from "../components/Footer.js";
 import "../../../src/global.css";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
 import Loading from "../loading.js";
+import { useAuthCheck } from "../hooks/useAuthCheck.js";
 
 export default function DashboardLayout({ children }) {
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const checkAuthentication = () => {
-      const isAuthenticated = JSON.parse(localStorage.getItem("isAuth"));
-      if (!isAuthenticated) {
-        router.push("/login");
-      } else {
-        setIsLoading(false);
-      }
-    };
-
-    checkAuthentication();
-  }, []);
+  const isLoading = useAuthCheck();
 
   // Render loading state until authentication check is done
   if (isLoading) {
