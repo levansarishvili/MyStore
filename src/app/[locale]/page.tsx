ca8eb1c6382dd984ebd048651946c85e57fb38cb
Levani Sarishvili <levanisarishvili322@gmail.com>
2024-12-21 22:32:36 +0400

    fix: solve internationalization logic issue

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,4 @@
 import { useTranslations } from "next-intl";
-import Link from "next/link";
 
 export default function HomePage() {
   const t = useTranslations("HomePage");
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,15 +2,15 @@ import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
 // For Supabase session management
-import { type NextRequest } from "next/server";
-import { updateSession } from "./utils/supabase/middleware";
-
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
-}
+// import { type NextRequest } from "next/server";
+// import { updateSession } from "./utils/supabase/middleware";
 
 export default createMiddleware(routing);
 
 export const config = {
   matcher: ["/", "/(ka|en)/:path*"],
 };
+
+// export async function middleware(request: NextRequest) {
+//   return await updateSession(request);
+// }
