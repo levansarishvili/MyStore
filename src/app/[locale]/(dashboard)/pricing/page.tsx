9c07d2d6c637a3954b858223f6663d3c300d05e8
Levani Sarishvili <levanisarishvili322@gmail.com>
2025-01-11 13:41:24 +0400

    refactor: update styles for better visibility with dark mode

--- src/app/[locale]/(dashboard)/pricing/page.tsx
+++ src/app/[locale]/(dashboard)/pricing/page.tsx
@@ -9,7 +9,7 @@ export default function Pricing({ params }: Props) {
   const locale = params.locale;
 
   return (
-    <div className="flex flex-col gap-8 rounded-2xl bg-[#f1f3f5] justify-center items-center max-w-[100rem] px-[4.8rem] py-[3.2rem]">
+    <div className="flex flex-col gap-8 rounded-2xl bg-[#f1f3f5] justify-center items-center max-w-[100rem] px-[4.8rem] py-[3.2rem] dark:bg-[#313131]">
       <h1 className="text-[2.2rem] font-semibold">Buy Subscription</h1>
 
       {/* Stripe */}
@@ -27,7 +27,7 @@ export default function Pricing({ params }: Props) {
             href={`/${locale}/donate-with-checkout`}
             className="card checkout-style-background"
           >
-            <h2 className="bottom text-[#202842] font-medium hover:text-[#ec5e2a] duration-300">
+            <h2 className="bottom text-[#202842] dark:text-white font-medium hover:text-[#ec5e2a] duration-300">
               Donate with hosted Checkout ➡
             </h2>
           </Link>
@@ -44,34 +44,30 @@ export default function Pricing({ params }: Props) {
 
       <div className="flex gap-36">
         {/* Free Plan */}
-        <div className="flex flex-col gap-8 items-start justify-between w-[32rem] p-8 bg-white rounded-lg">
+        <div className="flex flex-col gap-8 items-start justify-between w-[32rem] p-8 bg-white rounded-2xl dark:bg-[#313131] border dark:border-white text-[#202842] dark:text-white">
           <div className="flex flex-col">
-            <h2 className="text-[#202842] text-[2.2rem] font-semibold">
-              Free Plan
-            </h2>
+            <h2 className="text-[2.2rem] font-semibold">Free Plan</h2>
           </div>
-          <p className="text-[#202842] text-[2.6rem] font-medium">$0</p>
+          <p className="text-[2.6rem] font-medium">$0</p>
           <div className="flex flex-col gap-4">
             <div className="flex items-center gap-4">
               <div className="bg-[#ec5e2a] rounded-full flex justify-center items-center w-[2.6rem] h-[2.6rem] p-1">
                 <Check className="text-[#F4F2FD]" />
               </div>
-              <p className="text-[#202842] text-[1.4rem]">
-                View and buy products
-              </p>
+              <p className="text-[1.4rem]">View and buy products</p>
             </div>
 
             <div className="flex items-center gap-4">
               <div className="bg-[#ec5e2a] rounded-full flex justify-center items-center w-[2.6rem] h-[2.6rem] p-1">
                 <Check className="text-[#F4F2FD]" />
               </div>
-              <p className="text-[#202842] text-[1.4rem]">View blogs</p>
+              <p className=" text-[1.4rem]">View blogs</p>
             </div>
             <div className="flex items-center gap-4">
               <div className="bg-[#ec5e2a] rounded-full flex justify-center items-center w-[2.6rem] h-[2.6rem] p-1">
                 <Check className="text-[#F4F2FD]" />
               </div>
-              <p className="text-[#202842] text-[1.4rem]">Responsive Website</p>
+              <p className="text-[1.4rem]">Responsive Website</p>
             </div>
           </div>
           <div className="flex justify-center w-full">
@@ -82,57 +78,53 @@ export default function Pricing({ params }: Props) {
         </div>
 
         {/* Pro Plan */}
-        <div className="flex flex-col gap-8 items-start justify-between w-[32rem] p-8 bg-white rounded-lg">
+        <div className="flex flex-col gap-8 items-start justify-between w-[32rem] p-8 bg-white rounded-2xl dark:bg-[#313131] border dark:border-white text-[#202842] dark:text-white">
           <div className="flex flex-col">
-            <h2 className="text-[#202842] text-[2.2rem] font-semibold">
-              Pro Plan
-            </h2>
+            <h2 className="text-[2.2rem] font-semibold">Pro Plan</h2>
           </div>
-          <p className="text-[#202842] text-[2.6rem] font-medium">$49</p>
+          <p className="text-[2.6rem] font-medium">$49</p>
           <div className="flex flex-col gap-4">
             <div className="flex items-center gap-4">
               <div className="bg-[#ec5e2a] rounded-full flex justify-center items-center w-[2.6rem] h-[2.6rem] p-1">
                 <Check className="text-[#F4F2FD]" />
               </div>
-              <p className="text-[#202842] text-[1.4rem]">
-                View and buy products
-              </p>
+              <p className="text-[1.4rem]">View and buy products</p>
             </div>
             <div className="flex items-center gap-4">
               <div className="bg-[#ec5e2a] rounded-full flex justify-center items-center w-[2.6rem] h-[2.6rem] p-1">
                 <Check className="text-[#F4F2FD]" />
               </div>
-              <p className="text-[#202842] text-[1.4rem]">View blogs</p>
+              <p className="text-[1.4rem]">View blogs</p>
             </div>
             <div className="flex items-center gap-4">
               <div className="bg-[#ec5e2a] rounded-full flex justify-center items-center w-[2.6rem] h-[2.6rem] p-1">
                 <Check className="text-[#F4F2FD]" />
               </div>
-              <p className="text-[#202842] text-[1.4rem]">Responsive Website</p>
+              <p className="text-[1.4rem]">Responsive Website</p>
             </div>
             <div className="flex items-center gap-4">
               <div className="bg-[#ec5e2a] rounded-full flex justify-center items-center w-[2.6rem] h-[2.6rem] p-1">
                 <Check className="text-[#F4F2FD]" />
               </div>
-              <p className="text-[#202842] text-[1.4rem]">Edit products</p>
+              <p className="text-[1.4rem]">Edit products</p>
             </div>
             <div className="flex items-center gap-4">
               <div className="bg-[#ec5e2a] rounded-full flex justify-center items-center w-[2.6rem] h-[2.6rem] p-1">
                 <Check className="text-[#F4F2FD]" />
               </div>
-              <p className="text-[#202842] text-[1.4rem]">Add new products</p>
+              <p className="text-[1.4rem]">Add new products</p>
             </div>
             <div className="flex items-center gap-4">
               <div className="bg-[#ec5e2a] rounded-full flex justify-center items-center w-[2.6rem] h-[2.6rem] p-1">
                 <Check className="text-[#F4F2FD]" />
               </div>
-              <p className="text-[#202842] text-[1.4rem]">Edit blogs</p>
+              <p className="text-[1.4rem]">Edit blogs</p>
             </div>
             <div className="flex items-center gap-4">
               <div className="bg-[#ec5e2a] rounded-full flex justify-center items-center w-[2.6rem] h-[2.6rem] p-1">
                 <Check className="text-[#F4F2FD]" />
               </div>
-              <p className="text-[#202842] text-[1.4rem]">Add new blogs</p>
+              <p className="text-[1.4rem]">Add new blogs</p>
             </div>
           </div>
           <div className="flex justify-center w-full">
