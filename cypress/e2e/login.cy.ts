a8575196834c0c2fc4bde06aead77f069bec4b88
Levani Sarishvili <levanisarishvili322@gmail.com>
2025-01-17 02:32:39 +0400

    feat: add cypress tests for authorization

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,13 +1,27 @@
 describe("Authentication", () => {
+  // Login user test
   it("Logs in successfully", () => {
-    cy.visit("http://localhost:3000/login");
+    cy.visit("/login");
 
-    // cy.get("input[name=email]").type("testuser@gmail.com");
+    cy.get("input[name=email]").type("testuser@gmail.com");
 
-    // cy.get("input[name=password]").type("test123");
+    cy.get("input[name=password]").type("test123");
 
-    // cy.get("[data-cy='login-button']").click().click();
+    cy.get("[data-cy='login-button']").click();
 
-    // cy.url().should("include", "/profile");
+    cy.url().should("include", "/profile");
+  });
+
+  // Failed login test
+  it("Fails to log in", () => {
+    cy.visit("/login");
+
+    cy.get("input[name=email]").type("testuser@gmail.com");
+
+    cy.get("input[name=password]").type("incorrect_password");
+
+    cy.get("[data-cy='login-button']").click();
+
+    cy.url().should("not.include", "/profile");
   });
 });
