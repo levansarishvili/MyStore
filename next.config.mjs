42e8a29cab977bbd438dd3c76de37f2ee4a80333
Levani Sarishvili <levanisarishvili322@gmail.com>
2025-01-18 17:13:52 +0400

    refactor: update next config file

--- next.config.mjs
+++ next.config.mjs
@@ -8,18 +8,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn.dummyjson.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        pathname: "/**",
+        hostname: "**", // Allows all hostnames
+        pathname: "/**", // Allows any path
       },
     ],
   },
