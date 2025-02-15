7a5813256db03afdb35b08e957c9ccee7dc1e683
Levan Sarishvili <levanisarishvili322@gmail.com>
2025-02-15 23:04:46 +0400

    feat: install mui/material and create product rating system

--- src/app/[locale]/(dashboard)/store/[id]/page.tsx
+++ src/app/[locale]/(dashboard)/store/[id]/page.tsx
@@ -5,12 +5,24 @@ import { supabase } from "../../../../../lib/supabaseClient";
 import { ProductsType } from "../page";
 import GetUserData from "src/app/components/GetUserData";
 import SimilarProducts from "./SimilarProducts";
+import ProductReviews from "./ProductReviews";
 
 interface paramsType {
   params: {
     id: string;
     locale: string;
   };
+}
+
+export interface ReviewsType {
+  id: number;
+  created_at: string;
+  product_id: number;
+  review_text: string;
+  user_id: string;
+  rating: number;
+  likes: number | null;
+  dislikes: number | null;
 }
 
 // Fetch product data from API according to product ID
@@ -68,9 +80,22 @@ export default async function ProductDetailsPage({ params }: paramsType) {
     isInCart = true;
   }
 
+  // Fetch product reviews
+  const { data: reviews, error: reviewsError } = await supabase
+    .from("product_reviews")
+    .select("*")
+    .eq("product_id", id)
+    .order("created_at", { ascending: false });
+
+  if (reviewsError) {
+    console.error("Error fetching reviews:", reviewsError);
+    return null;
+  }
+
   return (
-    <section className="w-full max-w-[90rem] my-0 mx-auto px-6 md:px-12 lg:px-20 py-0">
+    <section className="w-full max-w-[90rem] my-0 mx-auto flex flex-col gap:6 md:gap-8 lg:gap-12 px-6 md:px-12 lg:px-20 py-0">
       <ProductDetails product={product} isInCart={isInCart} locale={locale} />
+      <ProductReviews id={id} userId={userId} reviews={reviews} />
       <SimilarProducts
         products={products}
         locale={locale}
